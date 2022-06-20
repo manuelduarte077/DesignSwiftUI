7d3cf515b93a6df0efb9f5487d4f90fb44ff927f
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-19 20:30:58 -0600

    initial commit

---
+++ Shared/DesignCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeSwiftUIApp.swift
+//  Shared
+//
+//  Created by Manuel Duarte on 19/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
