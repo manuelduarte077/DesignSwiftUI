8b035d51ab176b60c7737ab54ed7f8c5db3306b2
Manuel Duarte <carlosd99m@gmail.com>
2022-06-22 00:27:16 -0600

    Feature/animations (#5)
    * show view and transitions
    
    * matched geometry effect
    
    * delay and asymmetric transition

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,15 +8,72 @@
 import SwiftUI
 
 struct CoursesView: View {
-
+  
+  
+  @State var show = false
+  @Namespace var namespace
+  
   var body: some View{
-    Text("")
+    
+    ZStack {
+      ScrollView {
+        VStack (spacing: 20) {
+          CoursesItem()
+            .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
+            .frame(width: 335, height: 250)
+          CoursesItem()
+            .frame(width: 335, height: 250)
+        }
+        .frame(maxWidth: .infinity)
+        
+      }
+      
+      
+      if show {
+        ScrollView {
+          CoursesItem()
+            .matchedGeometryEffect(id: "Card", in: namespace)
+            .frame(height: 300)
+          
+          VStack {
+            ForEach(0 ..< 20) { item in
+              CourseRow()
+            }
+          }
+          .padding()
+          
+        }
+        .background(Color("Background 1"))
+        .transition(
+          .asymmetric(
+            insertion: AnyTransition
+              .opacity
+              .animation(Animation.spring()
+                .delay(0.3)),
+            removal: AnyTransition
+              .opacity
+              .animation(.spring())
+          )
+          
+          
+        )
+        .edgesIgnoringSafeArea(.all)
+      }
+      
+      
+    }
+    .onTapGesture {
+      withAnimation(.spring()) {
+        show.toggle()
+      }
+    }
+    
   }
   
 }
 
 struct CoursesView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoursesView()
-    }
+  static var previews: some View {
+    CoursesView()
+  }
 }
