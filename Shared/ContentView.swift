c135ec61c8b0d5ff8703706b414850d65c1387c1
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-22 22:48:38 -0600

    refactor code

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  @ViewBuilder
-  var body: some View {
-    
-    #if os(iOS)
-    Sidebar()
-    #else
-    Sidebar()
-      .frame(minWidth: 1000, maxWidth: 600)
-    #endif
-    
-  }
+	
+	@ViewBuilder
+	var body: some View {
+		
+#if os(iOS)
+		Sidebar()
+#else
+		Sidebar()
+			.frame(minWidth: 1000, maxWidth: 600)
+#endif
+		
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-    
-  }
+	static var previews: some View {
+		ContentView()
+		
+	}
 }
--- Shared/CourseRow.swift
+++ Shared/CourseRow.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct CourseRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
 }
 
 struct CourseRow_Previews: PreviewProvider {
-    static var previews: some View {
-        CourseRow()
-    }
+	static var previews: some View {
+		CourseRow()
+	}
 }
