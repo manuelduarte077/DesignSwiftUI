9839a4bb77e6f16dfb4d885ebff250a83a38afef
Manuel Duarte <carlosd99m@gmail.com>
2022-06-22 23:06:14 -0600

    refactor code (#6)
    * refactor code
    
    * matched cards and static data

--- Shared/CourseList.swift
+++ Shared/CourseList.swift
@@ -8,27 +8,27 @@
 import SwiftUI
 
 struct Courses: View {
-    @ViewBuilder
-    var body: some View {
-      #if os(iOS)
-          content
-            .listStyle(InsetGroupedListStyle())
-      #else
-          content
-            .frame(minWidth: 800, minHeight: 600)
-      #endif
-    }
-      
-    var content: some View {
-      List(0 ..< 20) { item in
-        CourseRow()
-      }
-      .navigationTitle("Courses")
-    }
+	@ViewBuilder
+	var body: some View {
+#if os(iOS)
+		content
+			.listStyle(InsetGroupedListStyle())
+#else
+		content
+			.frame(minWidth: 800, minHeight: 600)
+#endif
+	}
+	
+	var content: some View {
+		List(0 ..< 20) { item in
+			CourseRow()
+		}
+		.navigationTitle("Courses")
+	}
 }
 
 struct Courses_Previews: PreviewProvider {
-    static var previews: some View {
-        Courses()
-    }
+	static var previews: some View {
+		Courses()
+	}
 }
--- Shared/Sidebar.swift
+++ Shared/Sidebar.swift
@@ -8,62 +8,62 @@
 import SwiftUI
 
 struct Sidebar: View {
-  var body: some View {
-    NavigationView {
-      
-      #if os(iOS)
-      content
-        .navigationTitle("Learn")
-        .toolbar{
-          ToolbarItem(placement: .navigationBarTrailing) {
-            Image(systemName: "person.crop.circle")
-          }
-        }
-      #else
-      content
-        .frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
-        .toolbar{
-          ToolbarItem(placement: .automatic) {
-            Button(action: {}) {
-              Image(systemName: "person.crop.circle")
-            }
-          }
-        }
-      #endif
-      
-      CoursesView()
-      
-    }
-    
-  }
-  
-  var content: some View {
-    List {
-      NavigationLink(destination: CoursesView()) {
-        Label("Courses", systemImage: "book.closed")
-      }
-      NavigationLink(destination: CoursesView()) {
-        Label("Tutorials", systemImage: "list.bullet.rectangle")
-      }
-      NavigationLink(destination: CoursesView()) {
-        Label("Livestreams", systemImage: "tv")
-      }
-      NavigationLink(destination: CoursesView()) {
-        Label("Certificates", systemImage: "mail.stack")
-      }
-      NavigationLink(destination: CoursesView()) {
-        Label("Search", systemImage: "magnifyingglass")
-      }
-      
-    }
-    .listStyle(SidebarListStyle())
-    
-  }
-  
+	var body: some View {
+		NavigationView {
+			
+#if os(iOS)
+			content
+				.navigationTitle("Learn")
+				.toolbar{
+					ToolbarItem(placement: .navigationBarTrailing) {
+						Image(systemName: "person.crop.circle")
+					}
+				}
+#else
+			content
+				.frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
+				.toolbar{
+					ToolbarItem(placement: .automatic) {
+						Button(action: {}) {
+							Image(systemName: "person.crop.circle")
+						}
+					}
+				}
+#endif
+			
+			CoursesView()
+			
+		}
+		
+	}
+	
+	var content: some View {
+		List {
+			NavigationLink(destination: CoursesView()) {
+				Label("Courses", systemImage: "book.closed")
+			}
+			NavigationLink(destination: CoursesView()) {
+				Label("Tutorials", systemImage: "list.bullet.rectangle")
+			}
+			NavigationLink(destination: CoursesView()) {
+				Label("Livestreams", systemImage: "tv")
+			}
+			NavigationLink(destination: CoursesView()) {
+				Label("Certificates", systemImage: "mail.stack")
+			}
+			NavigationLink(destination: CoursesView()) {
+				Label("Search", systemImage: "magnifyingglass")
+			}
+			
+		}
+		.listStyle(SidebarListStyle())
+		
+	}
+	
 }
 
 struct Sidebar_Previews: PreviewProvider {
-  static var previews: some View {
-    Sidebar()
-  }
+	static var previews: some View {
+		Sidebar()
+	}
 }
