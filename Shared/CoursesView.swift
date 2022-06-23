a90df5d64570420f44231d7e263adfc4f115b769
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-22 23:05:44 -0600

    matched cards and static data

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -18,21 +18,21 @@ struct CoursesView: View {
 		ZStack {
 			ScrollView {
 				VStack (spacing: 20) {
-					CoursesItem()
-						.matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
-						.frame(width: 335, height: 250)
-					CoursesItem()
-						.frame(width: 335, height: 250)
+					ForEach(courses) { item in
+						CoursesItem(course: item)
+							.matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+							.frame(width: 335, height: 250)
+					}
+					
 				}
 				.frame(maxWidth: .infinity)
-				
 			}
 			
 			
 			if show {
 				ScrollView {
-					CoursesItem()
-						.matchedGeometryEffect(id: "Card", in: namespace)
+					CoursesItem(course: courses[0])
+						.matchedGeometryEffect(id: courses[0].id, in: namespace)
 						.frame(height: 300)
 					
 					VStack {
