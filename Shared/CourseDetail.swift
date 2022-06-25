3fe96530be0ed02081757ca266eab2d39e5ce8db
@manuelduarte077 <carlosd99m@gmail.com>
2022-06-25 13:17:02 -0600

    visual effect blur for macos

--- Shared/CourseDetail.swift
+++ Shared/CourseDetail.swift
@@ -12,31 +12,45 @@ struct CourseDetail: View {
 	var course: Course = courses[0]
 	var namespace: Namespace.ID
 	
+	#if os(iOS)
+		var cornerRadius: CGFloat = 22
+	#else
+		var cornerRadius: CGFloat = 10
+	#endif
 	
+	@ViewBuilder
 	var body: some View {
 		VStack {
-			ScrollView {
-				CoursesItem(course: course)
-					.matchedGeometryEffect(id: course.id, in: namespace)
-					.frame(height: 300)
-				
-				
-				VStack {
-					ForEach(courseSections) { item in
-						CourseRow()
-					}
+			
+		#if os(iOS)
+					content
+						.edgesIgnoringSafeArea(.all)
+		#else
+					content
+		#endif
+		}
+	}
+	
+	var content: some View {
+		ScrollView {
+			CoursesItem(course: course, cornerRadius: 0)
+				.matchedGeometryEffect(id: course.id, in: namespace)
+				.frame(height: 300)
+			
+			VStack {
+				ForEach(courseSections) { item in
+					CourseRow()
 				}
-				.padding()
-				
 			}
+			.padding()
 			
 		}
 		.background(Color("Background 1"))
-		.clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+		.clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
 		.matchedGeometryEffect(id: "container\(course.id)", in: namespace)
 		.edgesIgnoringSafeArea(.all)
-		
 	}
+	
 }
 
 struct CourseDetail_Previews: PreviewProvider {
--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CoursesView: View {
 	
 	
-	@State var show = false 
+	@State var show = false
 	@Namespace var namespace
 	@State var selectedItem: Course? = nil
 	@State var isDisabled = false
@@ -18,52 +18,75 @@ struct CoursesView: View {
 	var body: some View{
 		
 		ZStack {
-			ScrollView {
-				LazyVGrid (
-					columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
-						ForEach(courses) { item in
-							VStack {
-								CoursesItem(course: item)
-									.matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-									.frame(height: 200)
-									.onTapGesture {
-										withAnimation(.spring()) {
-											show.toggle()
-											selectedItem = item
-											isDisabled = true
-										}
+#if os(iOS)
+			content
+				.navigationBarHidden(true)
+			fullContent
+				.background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
+#else
+			content
+			fullContent
+				.background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
+#endif
+		}
+		.navigationTitle("Courses")
+		
+	}
+	
+	
+	var content: some View{
+		ScrollView {
+			LazyVGrid (
+				columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
+					ForEach(courses) { item in
+						VStack {
+							CoursesItem(course: item)
+								.matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+								.frame(height: 200)
+								.onTapGesture {
+									withAnimation(.spring()) {
+										show.toggle()
+										selectedItem = item
+										isDisabled = true
 									}
-									.disabled(isDisabled)
-							}
-							.matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
-							
+								}
+								.disabled(isDisabled)
 						}
+						.matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
 						
 					}
-					.padding(16)
-					.frame(maxWidth: .infinity)
-			}
-			.zIndex(1)
-			if selectedItem != nil {
-				ZStack(alignment: .topTrailing) {
-					CourseDetail (course:  selectedItem!, namespace: namespace)
 					
-					CloseButton()
-						.padding(.trailing, 16)
-						.onTapGesture {
-							withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
-								show.toggle()
-								selectedItem = nil
-								DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
-									isDisabled = false
-								}
+				}
+				.padding(16)
+				.frame(maxWidth: .infinity)
+		}
+		.zIndex(1)
+		
+	}
+	
+	
+	@ViewBuilder
+	var fullContent: some View {
+		
+		if selectedItem != nil {
+			ZStack(alignment: .topTrailing) {
+				CourseDetail (course:  selectedItem!, namespace: namespace)
+				
+				CloseButton()
+					.padding( 16)
+					.onTapGesture {
+						withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+							show.toggle()
+							selectedItem = nil
+							DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+								isDisabled = false
 							}
 						}
-				}
-				.zIndex(2)
+					}
 			}
-			
-			
+			.zIndex(2)
+			.frame(maxWidth: 712)
+			.frame(maxWidth: .infinity)
 		}
 		
 	}
