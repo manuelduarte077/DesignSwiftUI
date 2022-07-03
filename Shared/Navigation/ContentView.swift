519e3c8c1ee7258043ddd7253672504497a2dd64
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-03 15:54:19 -0600

    animating above navigation

--- Shared/Navigation/ContentView.swift
+++ Shared/Navigation/ContentView.swift
@@ -19,9 +19,9 @@ struct ContentView: View {
 	#if os(iOS)
 			
 			if horizontalSizeClass == .compact {
-				TabBar()
+				CoursesView()
 			} else {
-				Sidebar()
+                CoursesView()
 			}
 			
 	#else
