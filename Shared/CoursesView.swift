1b25f29816013c0824ddcd6fd8d24d8d1ed2a492
Manuel Duarte <carlosd99m@gmail.com>
2022-07-03 16:14:30 -0600

    animating above navigation (#9)

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,21 +8,24 @@
 import SwiftUI
 
 struct CoursesView: View {
-    
-    
     @State var show = false
     @Namespace var namespace
     @Namespace var namespace2
-    
     @State var selectedItem: Course? = nil
     @State var isDisabled = false
     
-    var body: some View{
-        
+#if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+#endif
+    
+    var body: some View {
         ZStack {
 #if os(iOS)
-            content
-                .navigationBarHidden(true)
+            if horizontalSizeClass == .compact {
+                tabBar
+            } else {
+                sidebar
+            }
             fullContent
                 .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
 #else
@@ -32,30 +35,22 @@ struct CoursesView: View {
 #endif
         }
         .navigationTitle("Courses")
-        
     }
     
-    var content: some View{
+    var content: some View {
         ScrollView {
-            
             VStack(spacing: 0) {
-                Text("Courses")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 16)
-                    .padding(.top, 54)
-            }
-            
-            LazyVGrid (
-                columns: [GridItem(.adaptive(minimum: 160), spacing: 16)], spacing: 16) {
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                    spacing: 16
+                ) {
                     ForEach(courses) { item in
                         VStack {
                             CoursesItem(course: item)
                                 .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
                                 .frame(height: 200)
                                 .onTapGesture {
-                                    withAnimation(.spring()) {
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
                                         show.toggle()
                                         selectedItem = item
                                         isDisabled = true
@@ -68,45 +63,43 @@ struct CoursesView: View {
                 }
                 .padding(16)
                 .frame(maxWidth: .infinity)
-            
-            Text("Latest sections")
-                .fontWeight(.semibold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding()
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 240))]) {
-                ForEach(courseSections) { item in
-                    #if os(iOS)
-                    NavigationLink(destination: CourseDetail(namespace: namespace2)){
+                
+                Text("Latest sections")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 240))]) {
+                    ForEach(courseSections) { item in
+#if os(iOS)
+                        NavigationLink(destination: CourseDetail(namespace: namespace2)) {
+                            CourseRow(item: item)
+                        }
+#else
                         CourseRow(item: item)
+#endif
                     }
-                    #else
-                    CourseRow(item: item)
-                    #endif
-                    
                 }
+                .padding()
             }
-            .padding()
         }
         .zIndex(1)
-        
+        .navigationTitle("Courses")
     }
-    
     
     @ViewBuilder
     var fullContent: some View {
-        
         if selectedItem != nil {
             ZStack(alignment: .topTrailing) {
-                CourseDetail (course:  selectedItem!, namespace: namespace)
+                CourseDetail(course: selectedItem!, namespace: namespace)
                 
                 CloseButton()
-                    .padding( 16)
+                    .padding(16)
                     .onTapGesture {
-                        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                        withAnimation(.spring()) {
                             show.toggle()
                             selectedItem = nil
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                 isDisabled = false
                             }
                         }
@@ -116,9 +109,77 @@ struct CoursesView: View {
             .frame(maxWidth: 712)
             .frame(maxWidth: .infinity)
         }
-        
     }
     
+    var tabBar: some View {
+        TabView {
+            NavigationView {
+                content
+            }
+            .tabItem {
+                Image(systemName: "book.closed")
+                Text("Courses")
+            }
+            
+            NavigationView {
+                Courses()
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.rectangle")
+                Text("Tutorials")
+            }
+            
+            NavigationView {
+                Courses()
+            }
+            .tabItem {
+                Image(systemName: "tv")
+                Text("Livestreams")
+            }
+            
+            NavigationView {
+                Courses()
+            }
+            .tabItem {
+                Image(systemName: "mail.stack")
+                Text("Certificates")
+            }
+            
+            NavigationView {
+                Courses()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var sidebar: some View {
+#if os(iOS)
+        NavigationView {
+            List {
+                NavigationLink(destination: content) {
+                    Label("Courses", systemImage: "book.closed")
+                }
+                Label("Tutorials", systemImage: "list.bullet.rectangle")
+                Label("Livestreams", systemImage: "tv")
+                Label("Certificates", systemImage: "mail.stack")
+                Label("Search", systemImage: "magnifyingglass")
+            }
+            .listStyle(SidebarListStyle())
+            .navigationTitle("Learn")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "person.crop.circle")
+                }
+            }
+            
+            content
+        }
+#endif
+    }
 }
 
 struct CoursesView_Previews: PreviewProvider {
